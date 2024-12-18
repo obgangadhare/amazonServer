13c7614e6408add759054f3f61af1524a8e550e9
Omkar Gangadhare <obgangadhare@gmail.com>
2024-12-18 18:01:24 +0530

    amazonServer

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use((req,res,next)=>{
 })
 
 app.use('/',routes);
-const url = "mongodb://localhost:27017/amazon";
+const url = "mongodb://127.0.0.1:27017/amazon";
 const PORT = 5020;
 mongoose.connect(
     url,
